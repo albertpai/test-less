a7de925d1b6da23dad28d35f9952b09fb416c2e9
albertpai <verdant.light@gmail.com>
2018-08-14 11:20:38 +0100

    second commit

---
+++ src/foo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './foo.less';
+
+export default class foo extends React.Component {
+    render() {
+        return (
+            <div className="cool">
+               <h1 className="foo">Fooooooooooooooooooooo</h1> 
+            </div>
+        );
+    }
+}
